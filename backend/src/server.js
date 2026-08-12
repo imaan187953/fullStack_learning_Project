4f8d7f0c2736c638f5bf35aea51fb2da2108ba5b
Eman Sajjad <akzachoudhry1123@gmail.com>
2026-08-12 19:16:49 +0500

    network error resolved

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,6 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Start Server
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
